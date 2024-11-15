500ab98638353c2f279ddda6a4d027b05ba14958
Nicat <webdevelopernicat@gmail.com>
2024-11-15 23:19:42 +0400

    Please GOD work this time please

---
+++ src/main/entity/abstracts/AbstractTicket.java
@@ -0,0 +1,13 @@
+package main.entity.abstracts;
+
+public abstract class AbstractTicket {
+  private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
